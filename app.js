9ce9c4a2fc29a598bdf5375575bc4ceb719ffe7f
EliasRC13 <eliasrc134531@gmail.com>
2024-09-04 10:33:17 -0300

    Arrays

--- app.js
+++ app.js
@@ -13,3 +13,26 @@ juegos.forEach((elemento, indice, arr) => {
     console.log(elemento, indice, arr);
     
     });
+
+let juegoNuevo = juegos.push('Mortal Kombat');
+
+console.log (juegoNuevo);
+console.log(juegos);
+
+let primerJuego = juegos.unshift('Batman Arkham City');
+
+console.log(juegos);
+
+juegos.pop();
+
+console.log(juegos);
+
+juegos.unshift('Mortal Kombat');
+
+console.log(juegos);
+
+juegos.splice(4, 1);
+
+console.log(juegos);
+
+console.log(juegos.indexOf('Zelda'));
