4298c6308ec2052f891e2e14879db1ac2062a916
EliasRC13 <eliasrc134531@gmail.com>
2024-09-12 08:52:38 -0300

    Practica Arrays

--- app2.js
+++ app2.js
@@ -1,9 +1,23 @@
 
 
-let equipos = ['Central', 'Velez', 'Racing'];
+let equipos = ['Rosario Central', 'Velez', 'Racing'];
 
 console.log(equipos.length);
 
 let equipoNuevo = equipos.push('San Lorenzo');
 
 console.log(equipos);
+
+equipos.pop();
+
+console.log(equipos);
+
+let equiposNuevos = equipos.push('Huracan', 'Lanus');
+
+console.log(equipos);
+
+equipos.splice(1, 3);
+
+console.log(equipos);
+
+console.log(equipos.indexOf('Rosario Central'));
