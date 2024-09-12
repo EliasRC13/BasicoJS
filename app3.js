5db23a91c1f68aff8047df0c1d5eeeb80eee258d
EliasRC13 <eliasrc134531@gmail.com>
2024-09-12 09:05:43 -0300

    Practica Objetos

---
+++ app3.js
@@ -0,0 +1,37 @@
+let personaje = {
+
+    nombre: 'Tony',
+    
+    codeName: 'IronMan',
+    
+    vivo: false,
+    
+    edad: 40,
+    
+    coord: {
+    
+    lat: 123,
+    
+    long: 424,
+    
+    },
+    
+    trajes: ['Mark I', 'Mark V', 'HulkBuster'],
+    
+    'ultima-pelicula': 'Infinity War',
+    
+    };
+
+    console.log(personaje.codeName);
+
+    console.log(personaje["ultima-pelicula"]);
+
+    console.log(personaje.trajes[2]);
+
+    let equipo = {
+        nombre: 'Rosario Central',
+        apodo: 'Canalla',
+        nacimiento: 1889,
+    }
+
+    console.log(equipo);
