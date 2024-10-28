cb215ccd8ff79821586f41c7da849c4f518948b9
EliasRC13 <eliasrc134531@gmail.com>
2024-10-27 21:35:43 -0300

    Practicando ingreso de header y footer

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const apellido = prompt("Por favor, ingresa tu apellido:");
 // Seleccionar el elemento del header por su ID y actualizar su contenido
 const headerSaludo = document.getElementById("saludo");
 headerSaludo.textContent = 'Hola, ' + nombre + ' ' + apellido +'! Bienvenido a nuestra página.';
+
+let Tony = {
+
+    nombre: 'Tony',
+    
+    codeName: 'IronMan',
+    
+    vivo: false,
+    
+    edad: 40,
+    
+    coord: {
+    
+    lat: 123,
+    
+    long: 424,
+    
+    },
+    
+    trajes: ['Mark I', 'Mark V', 'HulkBuster'],
+    
+    'ultima-pelicula': 'Infinity War',
+    
+    };
+
+    const footerHeroe = document.getElementById("iron");
+
+    footerHeroe.textContent = Tony.nombre + Tony.codeName;
